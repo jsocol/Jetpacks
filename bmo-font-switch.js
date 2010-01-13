71f458ead2eed5a43504f533b5f4a5559b36bf57
james <james@mail.jamessocol.com>
2010-01-12 22:37:52 -0600

    Adding comments to bmo-font-switch.js

--- bmo-font-switch.js
+++ bmo-font-switch.js
@@ -1,6 +1,17 @@
+/**
+ * BMO Font Switch
+ *
+ * Switches the monospace font on Bugzilla from the system
+ * default to Consolas.
+ *
+ * This Jetpack uses pageMods.
+ *
+ * http://jetpackgallery.mozillalabs.com/jetpacks/287
+ */
 jetpack.future.import('pageMods');
 
 jetpack.pageMods.add(function(document){
+	// Change the styles on <pre> elements.
     $(document).find('pre').each(function(i,pre){
         $(pre).css({"font-family":'Consolas, "Courier New", Courier, monospace',
                     "font-size":'14px'});
